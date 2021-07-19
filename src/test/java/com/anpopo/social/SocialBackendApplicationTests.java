520a88a73dd8bce1c30032e70d94b6803628d317
anpopo <dkstpgud@gmail.com>
2021-07-19 21:26:44 +0900

    다시 초기 커밋

---
+++ src/test/java/com/anpopo/social/SocialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anpopo.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
